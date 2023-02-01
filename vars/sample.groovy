2838ca3bcc1187d7f2b660673888b4eb356c2c39
jaindeepak2008 <jaindeepak2008@gmail.com>
2023-01-31 19:04:52 -0500

    Auto push robot

---
+++ vars/sample.groovy
@@ -0,0 +1,7 @@
+// This is how we declare a function in groovy.
+def info(message, URL) {
+    echo "INFO: ${message} and the URL is ${URL}"
+}
+
+// Calling the info function and supplying both the values. 
+info("DevOps", "DevOpsTraining.com")
