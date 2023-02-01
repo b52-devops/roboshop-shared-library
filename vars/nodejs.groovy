63d2798be738052f080ae1debc5a804772a1c768
jaindeepak2008 <jaindeepak2008@gmail.com>
2023-01-31 19:27:53 -0500

    Auto push robot

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,19 +1,28 @@
-pipeline{
-    agent any
-    stages{                                                     // Start of the stages
-        stage('Lint Checks') {
-            steps {
-                sh "echo Installing JSLINT"
-                sh "npm install jslint"
-                sh "ls -ltr node_modules/jslint/bin/"
-                // sh  "./node_modules/jslint/bin/jslint.js server.js"
-                sh "echo Lint checks completed........!!!!!"
+def lintChecks() {
+        sh "echo Installing JSLINT"
+        sh "npm install jslint"
+        sh "ls -ltr node_modules/jslint/bin/"
+        // sh "./node_modules/jslint/bin/jslint.js server.js"
+        sh "echo lint checks completed for ${COMPONENT}.....!!!!!"
+}
+
+def call()                                                           // call is the default function that's called by default.
+{
+    pipeline{
+        agent any
+        stages{                                                     // Start of the stages
+            stage('Lint Checks') {
+                steps {
+                    script {
+                        lintChecks()                                // If the function is in the same file, no need to call the function with the fileName as prefix.
+                    }
+                }
             }
-        }
-        stage('Downloading the dependencies') {
-            steps {
-                sh "npm install"
+            stage('Downloading the dependencies') {
+                steps {
+                    sh "npm install"
+                }
             }
-        }
-    }                                                // End of the stages
+        }                                                          // End of the stages
+    }
 }
