5123387f45d7aedfce840426ff40f2afdc842bc4
jaindeepak2008 <jaindeepak2008@gmail.com>
2023-02-07 20:37:23 -0500

    Auto push robot

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -8,7 +8,7 @@ def lintChecks(COMPONENT) {
 
 def sonarChecks(COMPONENT) {
         sh "echo Starting code quality analysis"
-        sh "sonar-scanner -Dsonar.host.url=http://${SONAR_URL}:9000 -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW}"
+        sh "sonar-scanner -Dsonar.host.url=http://${SONAR_URL}:9000 -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_USR} -Dsonar.password=password"
         sh "curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > quality-gata.sh"
         sh "quality-gata.sh ${SONAR_USR} ${SONAR_PWD} ${SONAR_URL} ${COMPONENT}"
 }
