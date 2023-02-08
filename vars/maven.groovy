484cf3e3ff23c465c5ea41385bf5bc6492001531
jaindeepak2008 <jaindeepak2008@gmail.com>
2023-02-07 21:08:54 -0500

    Auto push robot

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -5,10 +5,21 @@ def lintChecks(COMPONENT) {
         sh "echo lint checks completed for ${COMPONENT} .....!!!!!"
 }
 
+def sonarChecks(COMPONENT) {
+        sh "echo Starting code quality analysis"
+        sh "sonar-scanner -Dsonar.host.url=http://${SONAR_URL}:9000 -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW}"
+        sh "curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > quality-gata.sh"
+        sh "bash -x quality-gata.sh ${SONAR_USR} ${SONAR_PSW} ${SONAR_URL} ${COMPONENT}"
+}
+
 def call(COMPONENT)                                                           // call is the default function that's called by default.
 {
     pipeline{
         agent any
+        environment {
+            SONAR = credentials('SONAR')
+            SONAR_URL = "172.31.15.252"
+        }
         stages{                                                     // Start of the stages
             stage('Lint Checks') {
                 steps {
@@ -18,9 +29,17 @@ def call(COMPONENT)                                                           //
                 }
             }
 
+            stage('Sonar Checks') {
+                steps {
+                    script {
+                        sonarChecks(COMPONENT)                                // If the function is in the same file, no need to call the function with the fileName as prefix.
+                    }
+                }
+            }
+
             stage('Downloading the dependencies') {
                 steps {
-                    sh "npm install"
+                    sh "mvn clean package"
                 }
             }
         }                                                          // End of the stages
